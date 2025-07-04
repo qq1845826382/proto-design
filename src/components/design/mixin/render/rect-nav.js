69021f7c7e2bc7b7768a5f61f9e4bb2486402478
qq1845826382 <93645101+qq1845826382@users.noreply.github.com>
2025-07-04 14:51:00 +0800

    fix setting panel and default image size

--- src/components/design/mixin/render/rect-nav.js
+++ src/components/design/mixin/render/rect-nav.js
@@ -2,18 +2,32 @@
 import jsx from 'vue-jsx'
 import event from '@/core/event'
 // 引入流程控件图片
+// 引入所有图片，键名与组件类型一致
 import processImg from '@/../res/process.png'
+import PPSImg from '@/../res/PPS.png'
+import connectorImg from '@/../res/connector.png'
+import customerImg from '@/../res/customer.png'
+import fifoImg from '@/../res/fifo.png'
+import stockImg from '@/../res/stock.png'
 let {
   span,
   div
 } = jsx
 let _renderRectNav = function () {
   let me = this
-  // 仅保留流程控件
-  let retTags = ['process'].map(type => {
-    // 控件图标使用流程图片
+  // 根据图片数组生成所有可拖拽组件
+  let images = {
+    process: processImg,
+    PPS: PPSImg,
+    connector: connectorImg,
+    customer: customerImg,
+    fifo: fifoImg,
+    stock: stockImg,
+  }
+  let retTags = Object.keys(images).map(type => {
     let img = jsx.bind('img')
-    let children = [img({attrs_src: processImg, style_width: '24px', style_height: '24px'})]
+    // 使用对应图片作为控件图标
+    let children = [img({ attrs_src: images[type], style_width: '24px', style_height: '24px' })]
     return span({
       'class_label': true,
       on_mousedown (e) {
